91e365a42260a1a2ce9280c31bab311da25ba8ef
mmswebpage <70412032+mmswebpage@users.noreply.github.com>
2021-04-19 18:16:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+// 下部固定
+$(function() {
+    var topBtn = $('#nav');    
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+  });
+
+
+// スムーズスクロール
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      let speed = 500;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
+
